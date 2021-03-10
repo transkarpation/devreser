ce64bfb09f58c5a1e781ab9f46f01dedba09d38c
Boris <transkarpation@gmail.com>
2021-03-10 16:44:51 +0200

    top nav

---
+++ components/TopNav.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="top-nav">
+    TopNav
+    <ul class="menu">
+      <li class="menu-item-parent">
+        <a href="#"> женщинам </a>
+        <ul class="submenu">
+          <li class="submenu-item">одежда</li>
+          <li>обувь</li>
+          <li>аксессуары</li>
+          <li>коллекции</li>
+        </ul>
+      </li>
+      <li class="menu-item-parent">
+        <a href="#"> мужчинам </a>
+      </li>
+    </ul>
+    User corner
+  </div>
+</template>
